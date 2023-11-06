066a663236f2b1f46814bd11c7202251fde865d2
unknown <z@zuyvasevilla.com>
2023-11-06 15:52:40 -0700

    INTRO

---
+++ autoPlanter/src/autoPlanter.cpp
@@ -0,0 +1,20 @@
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+#include "Particle.h"
+
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+
+void setup() {
+}
+
+void loop() {
+ 
+}
