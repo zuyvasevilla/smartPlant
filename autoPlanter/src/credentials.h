6fddb6efa0ab8610b38e750a1cc012ef5760f406
unknown <z@zuyvasevilla.com>
2023-11-07 11:53:46 -0700

    FIXING CRED

--- autoPlanter/src/credentials.h
+++ autoPlanter/src/credentials.h
@@ -1,4 +1,4 @@
 #define AIO_SERVER "io.adafruit.com"
 #define AIR_SERVERPORT 1883
 #define AIO_USERNAME "zuyvasevilla"
-#define AIO_KEY "aio_VnPu90ngYlU5SgKSFYiq4LYvd8vv"
+#define AIO_KEY "aio_Mqev12bpztHssboVKXhgEeFsKhCD"
